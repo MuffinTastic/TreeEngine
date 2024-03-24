6ca83dce95621b99cc6db077825acaf198669fdd
MuffinTastic <muffintastic32@gmail.com>
2024-03-24 02:07:36 -0700

    add managedhost to sys.h/cpp

--- Source/Tree.NativeCommon/sys.cpp
+++ Source/Tree.NativeCommon/sys.cpp
@@ -6,10 +6,12 @@ namespace Tree::Sys
 {
 	static ICmdLineSystem* s_cmdLine = nullptr;
 	static ILogSystem* s_log = nullptr;
+	static IManagedHostSystem* s_managedHost = nullptr;
 	static IEngineSystem* s_engine = nullptr;
 
 	ICmdLineSystem* CmdLine() { return s_cmdLine; }
 	ILogSystem* Log() { return s_log; }
+	IManagedHostSystem* ManagedHost() { return s_managedHost; }
 	IEngineSystem* Engine() { return s_engine; }
 
 	void UpdateFromModule( Module* module )
@@ -18,6 +20,8 @@ namespace Tree::Sys
 			s_cmdLine = dynamic_cast<ICmdLineSystem*>( module->GetSystem( CMDLINESYSTEM_NAME ) );
 		if ( !s_log )
 			s_log = dynamic_cast<ILogSystem*>( module->GetSystem( LOGSYSTEM_NAME ) );
+		if ( !s_managedHost )
+			s_managedHost = dynamic_cast<IManagedHostSystem*>( module->GetSystem( MANAGEDHOSTSYSTEM_NAME ) );
 		if ( !s_engine )
 			s_engine = dynamic_cast<IEngineSystem*>( module->GetSystem( ENGINESYSTEM_NAME ) );
 	}
@@ -26,6 +30,7 @@ namespace Tree::Sys
 	{
 		s_engine = nullptr;
 		s_log = nullptr;
+		s_managedHost = nullptr;
 		s_cmdLine = nullptr;
 	}
 }
--- Source/Tree.NativeCommon/sys.h
+++ Source/Tree.NativeCommon/sys.h
@@ -2,6 +2,7 @@
 
 #include "Tree.Root/interfaces/icmdlinesystem.h"
 #include "Tree.Root/interfaces/ilogsystem.h"
+#include "Tree.Root/interfaces/imanagedhostsystem.h"
 #include "Tree.Root/interfaces/ienginesystem.h"
 
 namespace Tree
@@ -12,6 +13,7 @@ namespace Tree
 	{
 		ICmdLineSystem* CmdLine();
 		ILogSystem* Log();
+		IManagedHostSystem* ManagedHost();
 		IEngineSystem* Engine();
 
 		void UpdateFromModule( Module* module );
