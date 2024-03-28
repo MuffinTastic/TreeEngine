0525713dec630d081d8427b2945365ea956184e1
MuffinTastic <muffintastic32@gmail.com>
2024-03-28 16:21:57 -0700

    SapGen stub main func

---
+++ Source/Tree.SapGen/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree.SapGen;
+
+public static class Program
+{
+    public static void Main( string[] args )
+    {
+        Console.WriteLine( Directory.GetCurrentDirectory() );
+    }
+
+
+}
