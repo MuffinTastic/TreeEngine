f9a53de549fe802cc431c6eb6e4f419b1ec67dc0
MuffinTastic <muffintastic32@gmail.com>
2024-03-30 01:06:39 -0700

    use custom logger for c#

---
+++ Source/Tree.Engine/LogSystem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree.Engine;
+
+public static class LogSystem
+{
+    internal static Sap.Generated.ILogSystem ILogSystem { get; } = Sap.Generated.Sys.Log();
+
+    private static Dictionary<string, Logger> s_LoggerCache = new();
+
+    public static Logger CreateLogger( string name )
+    {
+        bool exists = s_LoggerCache.TryGetValue( name, out Logger? logger );
+
+        if ( exists )
+        {
+            return logger!;
+        }
+        else
+        {
+            logger = new Logger( ILogSystem.CreateLoggerSap( name ) );
+            s_LoggerCache.Add( name, logger );
+            return logger;
+        }
+    }
+}
---
+++ Source/Tree.Engine/Logger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree.Engine;
+
+public class Logger
+{
+    private Sap.Generated.ILogger _logger;
+
+    internal Logger( Sap.Generated.ILogger logger )
+    {
+        _logger = logger;
+    }
+
+    public void Info( string text,
+        [CallerMemberName] string memberName = "",
+        [CallerFilePath] string sourceFilePath = "",
+        [CallerLineNumber] int sourceLineNumber = 0 )
+    {
+        _logger.InternalInfo( sourceFilePath, sourceLineNumber, memberName, text );
+    }
+
+    public void Warning( string text,
+        [CallerMemberName] string memberName = "",
+        [CallerFilePath] string sourceFilePath = "",
+        [CallerLineNumber] int sourceLineNumber = 0 )
+    {
+        _logger.InternalWarning( sourceFilePath, sourceLineNumber, memberName, text );
+    }
+
+    public void Error( string text,
+        [CallerMemberName] string memberName = "",
+        [CallerFilePath] string sourceFilePath = "",
+        [CallerLineNumber] int sourceLineNumber = 0 )
+    {
+        _logger.InternalError( sourceFilePath, sourceLineNumber, memberName, text );
+    }
+}
