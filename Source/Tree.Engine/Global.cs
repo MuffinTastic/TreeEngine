71adfb448fc241c7949399d430eb0a9fafc9e8e5
MuffinTastic <muffintastic32@gmail.com>
2024-03-30 01:07:17 -0700

    global usings, allows LoggerExtensions

---
+++ Source/Tree.Engine/Global.cs
@@ -0,0 +1,8 @@
+﻿global using static Tree.Engine.Global;
+
+namespace Tree.Engine;
+
+public static class Global
+{
+    public static Logger Log { get; } = LogSystem.CreateLogger( "Managed" );
+}
---
+++ Source/Tree.Trunk/Global.cs
@@ -0,0 +1,3 @@
+﻿global using static Tree.Engine.Global;
+
+namespace Tree.Trunk;
