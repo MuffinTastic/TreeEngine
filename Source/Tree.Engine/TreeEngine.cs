c59ee0c178f63d78c127164a6e372ca9e55da6a3
MuffinTastic <muffintastic32@gmail.com>
2024-04-04 02:52:11 -0700

    TreeEngine formatting

--- Source/Tree.Engine/TreeEngine.cs
+++ Source/Tree.Engine/TreeEngine.cs
@@ -9,13 +9,12 @@ namespace Tree.Engine;
 
 public static class TreeEngine
 {
-    internal static IEngineSystem IEngineSystem { get; } = Sys.Engine();
-    public static EDomain s_Domain { get; } = IEngineSystem.GetDomain();
+    internal static IEngineSystem s_EngineSystem { get; } = Sys.Engine();
+    internal static EDomain s_Domain { get; } = s_EngineSystem.GetDomain();
 
     
     public static bool IsClient => ( s_Domain & EDomain.EDOMAIN_CLIENT ) == EDomain.EDOMAIN_CLIENT;
     public static bool IsClientOnly => s_Domain == EDomain.EDOMAIN_CLIENT;
     public static bool IsEditorOnly => ( s_Domain & EDomain.EDOMAIN_EDITOR_ONLY ) == EDomain.EDOMAIN_EDITOR_ONLY;
     public static bool IsDedicatedServer => s_Domain == EDomain.EDOMAIN_DEDICATED_SERVER;
-
 }
